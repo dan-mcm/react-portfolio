03cef5e7ed39d7e51ffc4be847b6fd1325131d73
daniel40392 <daniel40392@gmail.com>
2018-05-01 22:39:23 +0100

    added default height to cards for consistency

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { Flex, Box } from "grid-styled";
 
-const Structure = styled.div``;
+const Structure = styled.div`
+  min-height: 400px;
+  margin: 1em;
+`;
 
 const CardTitle = styled.h1`
   color: darkred;
